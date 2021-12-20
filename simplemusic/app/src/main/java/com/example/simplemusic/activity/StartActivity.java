0ddae6d8322085871858566c73b9c7d879e80e86
zhylng <1020752797@qq.com>
2021-12-20 16:01:15 +0800

    zhylng提交

---
+++ simplemusic/app/src/main/java/com/example/simplemusic/activity/StartActivity.java
@@ -0,0 +1,31 @@
+package com.example.simplemusic.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+
+import com.example.simplemusic.R;
+//开始广告页
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_start);
+
+        Handler handler = new Handler();
+        // 延迟SPLASH_DISPLAY_LENGHT时间然后跳转到MainActivity
+        int SPLASH_DISPLAY_LENGHT = 1000;
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(StartActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGHT);
+    }
+}
